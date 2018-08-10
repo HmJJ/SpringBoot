68015bc973c728b556fa0769b2fbfef10dc29298
Nott <1564223954@qq.com>
2018-08-10 16:08:01 +0800

    Update Init.java

--- src/main/java/com/springboot/boot/Init.java
+++ src/main/java/com/springboot/boot/Init.java
@@ -3,6 +3,7 @@ package com.springboot.boot;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
+@Component
 public class Init implements ApplicationRunner {
 
 	@Override
